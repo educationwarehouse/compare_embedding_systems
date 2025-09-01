749825fa4ff365a8f715bcaa86ea4eca822b3df3
remco <remco.b@educationwarehouse.nl>
2025-09-01 14:12:38 +0200

    fix: gextraheerde modellen

---
+++ models_to_test.py
@@ -0,0 +1,15 @@
+# Hardcoded lijst van modellen om te testen
+MODELS_TO_TEST = [
+    "nomic-embed-text",
+    "mxbai-embed-large",
+    "all-minilm",
+    "bge-large",
+    'bge-m3',
+    'V4lentin1879/jina-bert-code-f16',
+    'olfh/teuken-7b-instruct-commercial-v0.4:7b-instruct-q4_K_M',
+    'olfh/teuken-7b-instruct-commercial-v0.4:7b-instruct-q8_0',
+    'snowflake-arctic-embed2',
+    'jobautomation/OpenEuroLLM-Dutch',
+]
+
+
